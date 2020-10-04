4c6f79f98a42ae7fce5b6cc232e875510152c4da
Shannon Hong <shannon1009@gmail.com>
2020-10-04 01:21:30 -0700

    add styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Airtable from "airtable";
 import Goal from "./components/Goal";
+import styled from "styled-components";
+import { GlobalStyle } from "./styles/Global.style";
 
 const base = new Airtable({ apiKey: "keyXg62pIdPwifjO5" }).base(
   "appCjonADKoha9fSS"
 );
+
+const StyledH1 = styled.h1`
+  text-align: center;
+  font-size: 4rem;
+  margin: 1rem 0;
+`;
 
 function App() {
   const [goals, setGoals] = useState([]);
@@ -26,7 +34,8 @@ function App() {
   }, []);
   return (
     <>
-      <h1> My Goals </h1>
+      <GlobalStyle />
+      <StyledH1> My Goals </StyledH1>
       {goals.map((goal) => (
         <Goal
           key={goal.id}
--- src/components/Goal.js
+++ src/components/Goal.js
@@ -1,21 +1,26 @@
 import React from "react";
+import StyledGoal from "../styles/StyledGoal";
+import StyledCheckBox from "../styles/StyledCheckBox";
+import StyledGoalDetails from "../styles/StyledGoalDetails";
 
 const Goal = ({ goal, updates }) => {
   return (
-    <div>
-      <label>
-        <input type="checkbox" defaultChecked={goal.fields.complete} disabled />
-      </label>
+    <StyledGoal>
+      <StyledCheckBox>
+        {" "}
+        <input type="checkbox" defaultChecked={goal.fields.complete} />
+        <span />
+      </StyledCheckBox>
       <h2>{goal.fields.title}</h2>
-      <div>
+      <StyledGoalDetails>
         <h3>DETAILS</h3>
         <p>{goal.fields.details}</p>
         <h3>UPDATES</h3>
         {updates.map((update, index) => (
           <p key={index}>{update.fields.update}</p>
         ))}
-      </div>
-    </div>
+      </StyledGoalDetails>
+    </StyledGoal>
   );
 };
 
